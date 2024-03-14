fb89b41144dff4f3f3bb398da8650c33facf825c
usagi69 <hei.taratra@gmail.com>
2024-03-14 14:10:44 +0300

    Update battleDev1.test.js

--- Test/battleDev1.test.js
+++ Test/battleDev1.test.js
@@ -2,7 +2,6 @@ import { expect } from "chai";
 import { describe, it } from "mocha";
 import { level1 } from "../battleDev1.js";
 
-
 describe('Voici le test pour le Battle Dev level1', () => {
     describe('1 / Quelques test simples pour tester le fonctionnement de base: ', function () {
         it('Test avec un parent A possédant un gène récessif',() =>{
@@ -14,21 +13,13 @@ describe('Voici le test pour le Battle Dev level1', () => {
         it('Test avec un parent A entièrement récessif et un parent B entièrement dominant ',() =>{
             expect(level1(['ABC', 'XYZ'])).to.deep.equal('XYZ');
         })
-    })
-    describe('2 / Quelques test plus avancés : ', function () {
-        it('Test avec un parent A possédant un gène récessif',() =>{
-            expect(level1(['AYC', 'AYZ'])).to.deep.equal('AYZ');
+        it('Test avec un parent B possédant un gène récessif',() =>{
+            expect(level1(['ABZ', 'XYC'])).to.deep.equal('XYZ');
         })
-        it('Test avec un parent A possédant deux gènes récessif et un parent B avec deux gènes dominantes',() =>{
-            expect(level1(['ABC', 'AYZ'])).to.deep.equal('AYZ');
-        })
-        it('Test avec un parent A entièrement récessif et un parent B entièrement dominant ',() =>{
-            expect(level1(['ABC', 'XYZ'])).to.deep.equal('XYZ');
+        it('Test avec un parent A partiellement récessif',() =>{
+            expect(level1(['XBZ', 'AYC'])).to.deep.equal('XYZ');
         })
     })
-
-
-
 
 
 })
