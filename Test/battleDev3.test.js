626da848e330f8110e71e14e2827ffbbca50ac2e
usagi69 <hei.taratra@gmail.com>
2024-03-14 15:11:04 +0300

    add level3

--- Test/battleDev3.test.js
+++ Test/battleDev3.test.js
@@ -4,8 +4,17 @@ import { level3 } from '../battleDev3.js'
 
 describe('Voici les tests pour le battle Dev level3', () => {
     describe('Quelques tests simples', () => {
+        it('Test avec 3 indications', () => {
+            expect(level3([3, 'ADD 5', 'SWAP', 'ADD 10'])).to.deep.equal('5 10')
+        })
         it('Test avec 6 indications', () => {
             expect(level3([6, 'ADD 5', 'SWAP', 'ADD 3', 'JUMP -2', 'TRANSFER', 'ADD 2'])).to.deep.equal('11 2')
+        })
+        it('Test avec 10 indications contenant JUMP 0', () => {
+            expect(level3([10, 'ADD 5', 'SWAP', 'ADD 3', 'JUMP -2', 'TRANSFER', 'ADD 2', 'ADD 5', 'SWAP', 'JUMP 0', 'ADD 2'])).to.deep.equal('7 13')
+        })
+        it('Test avec 12 indications', () => {
+            expect(level3([12, 'ADD 5', 'SWAP', 'ADD 3', 'JUMP -2', 'TRANSFER', 'ADD 2', 'ADD 5', 'SWAP', 'JUMP 0', 'ADD 2', 'TRANSFER', 'ADD 12'])).to.deep.equal('20 12')
         })
     })
 
---
+++ battleDev3.js
@@ -0,0 +1,42 @@
+/**
+ *  permet de déterminer le volume contenu dans chacun des deux réservoirs à la fin
+ * de la recette.
+ * @param {[]} input un tableau avec le format suivant: 
+ * le premier élement du tableau correspond au nombre d'instruction de la recette,
+ * le reste du tableau correspond au instruction à suivre .
+ * @returns deux entier séparés par un esapce: le volume contenu à la fin de la 
+ * recette dans le bac de gauche et de droite respectivement.
+ */
+
+export function level3 (input){
+    let number_of_instructions = input.shift();
+    let left = 0;
+    let right = 0;
+    let jump_instructions = []
+
+    for (let i = 0; i < number_of_instructions; i++){
+        let instruction = input[i].split(' ')
+        if (instruction[0] == 'ADD'){
+            right += parseInt(instruction[1]);
+        }
+        if (instruction[0] == 'TRANSFER'){
+            left += right;
+            right = 0;
+        }
+        if (instruction[0] == 'SWAP'){
+            [left, right] = [right , left];
+        }
+        if (instruction[0] == 'JUMP' && jump_instructions.includes(i) == false ){
+            jump_instructions.push(i)
+            let jump =  parseInt(instruction[1])
+            if (jump != 0){
+                i = i + jump - 1;
+            }
+        }
+    }
+    return `${left} ${right}`;
+}
+
+// let test1 = level3([6,'ADD 5', 'SWAP', 'ADD 3', 'JUMP -2', 'TRANSFER', 'ADD 2'])
+// console.log(test1);
+
